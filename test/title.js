5fd4c35e414f9c046c1e7ecc22dc4a6a166222e7
EndoHizumi <endohizumi0114@gmail.com>
2024-02-27 23:32:35 +0900

    テスト用のゲームファイルを用意した

---
+++ test/title.js
@@ -0,0 +1,9 @@
+let index = 0
+export const sceneConfig = {
+  background: 'back.jpg'
+}
+export const scenario = [{ type: 'text', msg: '夏の陽気が残る９月の初旬' }]
+// 他のロジック
+export const skipScenario = () => {
+  index = 1
+}
