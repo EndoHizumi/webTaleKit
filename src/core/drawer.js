1538cf01797d9e770f509bf3e13112a5427933c5
EndoHizumi <endohizumi0114@gmail.com>
2024-02-28 00:29:10 +0900

    一行だけ文字を表示できるようになった

--- src/core/drawer.js
+++ src/core/drawer.js
@@ -20,38 +20,42 @@ export class Drawer {
 
   setConfig (config) {
     // 背景画像をcanvasに描画する
+    console.log(config)
     this.config = config
     const img = new Image()
     img.src = this.config.background
+    console.dir(img)
     img.onload = () => {
+      console.log('load')
       this.ctx.drawImage(img, 0, 0, this.ctx.width, this.ctx.height)
     }
   }
 
   drawText (scene) {
     // テキストを描画する(座標は、読み込んだUIのHTMLのmessageBox要素に合わせる)
-    // pタグをmessageBoxに追加する。
-    const messageText = document.createElement('p')
-    document.getElementById('messageBox').appendChild(messageText)
     // テキストは一文字ずつ描画する
+    this.messageText.innerHTML = ''
     this.ctx.font = '20px serif'
     this.ctx.fillStyle = 'black'
-    scene.text.split('').forEach((char, i) => {
+    scene.msg.split('').forEach((char, i) => {
       setTimeout(() => {
-        messageText.innerHTML = messageText.innerHTML + char
+        this.messageText.innerHTML = this.messageText.innerHTML + char
       }, 100 * i)
     })
   }
 
-  drawUI() {
+  async drawUI () {
     // UIを描画する
     // UI用のHTMLを読み込んで、DOMに追加する
     const ui = document.createElement('div')
-    ui.innerHTML = axios
-      .get('hhttp://127.0.0.1:8080/screen.html')
+    ui.innerHTML = await axios
+      .get('http://127.0.0.1:8080/screen.html')
       .then((res) => {
         return res.data
       })
     document.body.appendChild(ui)
+    // pタグをmessageBoxに追加する。
+    this.messageText = document.createElement('p')
+    document.getElementsByClassName('message-window')[0].appendChild(this.messageText)
   }
 }
--- src/core/main.js
+++ src/core/main.js
@@ -10,33 +10,30 @@ export class Core {
     // ScenarioManagerの初期化（変数の初期値設定）
     this.scenarioManager = new ScenarioManager()
     // ResourceManagerの初期化（引数にconfig.jsを渡して、リソース管理配列を作る）
-    this.resourceManager = new ResourceManager(import('./config.js'))
+    this.resourceManager = new ResourceManager(import(/* webpackIgnore: true */ './config.js')) //  webpackIgnoreでバンドルを無視する
     this.isNext = false
   }
 
   async init () {
     // titleタグの内容を書き換える
     document.title = engineConfig.title
-    const scenario = await import('./title.js')
-    this.drawer.setConfig(scenario.sceneConfig)
+    this.drawer.drawUI()
+    const title = await import(/* webpackIgnore: true */ './title.js')//  webpackIgnoreでバンドルを無視する
+    this.drawer.setConfig(title.sceneConfig)
     // scenario配列をmapで処理して、ゲームを進行する。
-    scenario.forEach((scene) => {
+    title.scenario.forEach((scene) => {
       // type:textの場合, DrawerのdrawTextメソッドを実行する
+      console.log(scene)
       if (scene.type === 'text') {
         this.drawer.drawText(scene)
         this.scenarioManager.setHistory(scene.text)
         // waitがある場合、クリック待ちをする
         if (scene.wait) {
-          window.addEventListener('keydown', (this.isNext = true))
-          window.addEventListener('click', (this.isNext = true))
-          while (true) {
-            //  キーボード入力またはマウスがクリックされるまで無限ループ
-            if (this.isNext) {
-              window.removeEventListener('keydown')
-              window.removeEventListener('click')
-              break
-            }
-          }
+          this.isNext = false
+          this.drawer.messageText.addEventListener('click', () => {
+            this.isNext = true
+            // うまくいってない。クリック待ちをSetTimeoutで実装する？
+          })
         }
       }
     })
