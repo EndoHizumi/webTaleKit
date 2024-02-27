520aef4bf18de6416dbac56ae42a14afcd7f10ab
EndoHizumi <endohizumi0114@gmail.com>
2024-02-27 23:31:00 +0900

    webPackを設定した

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js', // エントリーポイントのファイル
+  output: {
+    filename: 'bundle.js', // 出力ファイル名
+    path: path.resolve(__dirname, 'dist'), // 出力ディレクトリ
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html', // (オプション) 使用するHTMLテンプレートファイル
+      filename: 'index.html', // 出力されるHTMLファイル名
+    }),
+  ],
+};
