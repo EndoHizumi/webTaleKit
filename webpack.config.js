cfdaf9f18e4da232309613d3b1de4eee0b60b3a2
EndoHizumi <endohizumi0114@gmail.com>
2024-03-17 00:55:02 +0900

    typeScriptに変更

--- webpack.config.js
+++ webpack.config.js
@@ -18,4 +18,17 @@ module.exports = {
     static: './dist',
     port: 8080,
   },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  devtool: 'inline-source-map'
 };
