1741d0fcfcbe458e70618338c2887671808028b9
EndoHizumi <endohizumi0114@gmail.com>
2024-06-29 19:30:44 +0900

    コマンドからloaderをビルドできるようにした

--- loader/cli.js
+++ loader/cli.js
@@ -1,11 +1,12 @@
 const parse = require('./parser.js')
 const fs = require('fs')
+const path = require('path')
 /**
  * WebTaleScript Parser CLI
  */
 // ファイル名を取得する。（拡張子を除く）
 const targetScript = process.argv.slice(2)[0]
-const fileName = targetScript.split('.')[0]
+const fileName = path.basename(targetScript).split('.')[0]
 const outputPath = process.argv.slice(2)[1] || ''
 // 末尾にスラッシュがない場合、追加する
 if (outputPath.length > 0) {
@@ -20,8 +21,9 @@ fs.readFile(targetScript, 'utf8', async (err, data) => {
     console.error(err)
     return
   }
+  console.log(data)
   // パーサーを呼び出す。
-  const { logic, scenario } = await parse(data)
+  const { scenario, logic } = await parse(data)
   fs.writeFile(
     `${outputPath}${fileName}.js`,
     `${logic};\nexport const scenario = ${JSON.stringify(scenario)}; `,
